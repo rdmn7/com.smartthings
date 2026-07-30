675b513f4a4e0da3cf914f321d4f1ff671274ee6
RDMN <22493741+rdmn7@users.noreply.github.com>
2026-07-30 19:51:54 +0200

    feat: extend dryer controls and metering

--- drivers/dryer/driver.js
+++ drivers/dryer/driver.js
@@ -2,25 +2,39 @@
 
 const SmartThingsDriver = require('../../lib/SmartThingsDriver');
 
-module.exports = class SmartThingsDriverDryer extends SmartThingsDriver { 
-   
- async onOAuth2Init() {
+module.exports = class SmartThingsDriverDryer extends SmartThingsDriver {
+
+  async onOAuth2Init() {
     await super.onOAuth2Init();
 
     this.homey.flow.getActionCard('set_dryer_state')
       .registerRunListener(async ({ device, state }) => {
-        const selectedState = state; // "run", "pause" or "stop"
         await device.executeCommand({
           component: 'main',
           capability: 'dryerOperatingState',
           command: 'setMachineState',
-          args: [selectedState],
-        });       
+          args: [state],
+        });
+      });
+
+    this.homey.flow.getActionCard('set_dryer_cycle')
+      .registerRunListener(async ({ device, cycle }) => {
+        await device.executeCommand({
+          component: 'main',
+          capability: 'samsungce.dryerCycle',
+          command: 'setDryerCycle',
+          args: [cycle],
+        });
+      });
+
+    this.homey.flow.getConditionCard('samsung_dryer_remote_control_is_enabled')
+      .registerRunListener(async ({ device }) => {
+        return device.getCapabilityValue('samsung_dryer_remote_control_enabled') === true;
       });
   }
+
   onPairFilterDevice(device) {
     this.log('onPairFilterDevice', device.deviceTypeName);
-
     if (device.deviceTypeName === 'Samsung OCF Dryer') return true;
     return false;
   }
